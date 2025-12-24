f4ca9197587d18850931b027649198192cc69de5
Saral Chaudhary <saralshive2411@gmail.com>
2025-12-24 16:09:49 +0530

    added loggers in sum and modified code

--- Sum.java
+++ Sum.java
@@ -2,7 +2,9 @@ public class Sum{
     public static void main(String [] args){
         int a =5;
         int b=6;
+        int sum = a + b;
 
         System.out.println(a+b);
+        System.out.println("Sum of 2 number is "+ sum);
     }
 }
