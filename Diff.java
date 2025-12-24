7e7e98e4b2d0083f81592bab108ea835367d1cce
Saral Chaudhary <saralshive2411@gmail.com>
2025-12-24 16:06:22 +0530

    adding diff java file to staging

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff{
+    public static void main(String [] args){
+        int a=55;
+        int b=45;
+        int diff = a-b;
+        System.out.println("The diff is "+ diff)
+    }
+}
