25563538991de73af4d072584f311c5f2e246ec2
Saral Chaudhary <saralshive2411@gmail.com>
2025-12-24 17:04:20 +0530

    Commiting files in my branch for multiplication

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply{
+    public static void main(String [] args){
+        int a = 15;
+        int b= 16;
+        int mul = a * b;
+
+        System.out.println("The multiply of 2 number are "+mul);
+    }
+}
